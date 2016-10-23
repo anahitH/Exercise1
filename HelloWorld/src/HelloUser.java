1a2c8e3bb8046a19db6f41b403f366719c285a0a
anahitH <anahithayrapetyan91@gmail.com>
2016-10-23 20:43:40 +0200

    Added HelloUser class.

---
+++ HelloWorld/src/HelloUser.java
@@ -0,0 +1,14 @@
+
+public class HelloUser {
+	private String userName;
+
+	public HelloUser(String name)
+	{
+		userName = name;
+	}
+	
+	public void greetUser()
+	{
+		System.out.println("Hello " + userName);
+	}
+}
