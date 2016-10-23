b618682bfb86cc8610e00c6e356a48168c9c9ee8
anahitH <anahithayrapetyan91@gmail.com>
2016-10-23 20:58:16 +0200

    Modified HelloWord class

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * 
  */
@@ -10,9 +14,17 @@ public class HelloWorld {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Hello ASE2016 - how are you? :)");
+		System.out.println("Please enter your name: ");
+		
+		BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
+		String userName = inReader.readLine();
+		
+		HelloUser helloUser = new HelloUser(userName);
+		helloUser.greetUser();
 	}
 
 }
